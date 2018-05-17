0df31f0c0e80eed1c5d528941d81b34ee39376dd
kre <krexspace@gmail.com>
2018-05-17 19:38:33 +0000

    docker auto start enabled

--- docker/mongo_docker.sh
+++ docker/mongo_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run --name p-mongo -p 27017:27017 -v ${PWD}/data_mongo:/data/db -d mongo
+sudo docker run --restart=always --name p-mongo -p 27017:27017 -v ${PWD}/data_mongo:/data/db -d mongo
